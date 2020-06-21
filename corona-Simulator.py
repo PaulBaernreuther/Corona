c1025919d37bac065a36fe9ddf666afbd9c39d5e
PaulBaernreuther <65441399+PaulBaernreuther@users.noreply.github.com>
2020-06-21 22:46:07 +0200

    Add files via upload

--- corona-Simulator.py
+++ corona-Simulator.py
@@ -10,25 +10,42 @@ from rooms import *
 
 
 class scenario:
-    def __init__(self):
+    def __init__(self, infection_rate = 0.2, number_infected = 1, deathrate = 0.1, max_infected_time = 7):
         self.number_of_rooms = 5
         self.rooms = []
         self.opt_arangement = 0,0
         self.shape = (20,20)
         self.members = 100
+        self.infection_rate = infection_rate
+        self.number_infected = number_infected
+        self.deathrate = deathrate
+        self.max_infected_time = max_infected_time
+        self.current_arangement = [0,0]
     def find_opt_arangement(self):
         a, b = fig.get_size_inches()
         return find_opt_arangement(self.number_of_rooms, ratio=(self.shape[1] * a / 2) / (self.shape[0] * b))
     def create_rooms(self):
         i,j = self.find_opt_arangement()
+        self.current_arangement = [i,j]
         for l in range(self.number_of_rooms):
-            newroom = room(self.shape)
+            newroom = room(self.number_infected, self.shape)
             newroom.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
             newroom.compute_scale(fig)
+            the_infected = random.sample(range(self.members), self.number_infected)
             for m in range(self.members):
-                person = Person(newroom)
+                if m in the_infected:
+                    person = Person(newroom, infected = True)
+                else:
+                    person = Person(newroom)
                 person.register()
             self.rooms.append(newroom)
+    def update_room_axes(self):
+        i,j = self.find_opt_arangement()
+        if i != self.current_arangement[0] or j != self.current_arangement[1]:
+            for l, room in enumerate(self.rooms):
+                room.ax.remove()
+                room.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
+            self.current_arangement = [i, j]
     def draw_all_rooms(self):
         for thisroom in self.rooms:
             thisroom.draw()
@@ -45,6 +62,35 @@ class scenario:
             #room.ax.draw_artist(room.scatter)
             #room.ax.draw_artist(room.scatter2)
             #fig.canvas.blit(room.ax.bbox)
+
+    def calculate_infected(self):
+        for space in self.rooms:
+            for prsn in space.persons:
+                if prsn.status == "i":
+                    for otherprsn in space.persons:
+                        if otherprsn.status == "v":
+                            distance = np.linalg.norm(np.array(prsn.position) - np.array(otherprsn.position))
+                            if distance <= prsn.radius or distance <= otherprsn.radius:
+                                if random.random() <= self.infection_rate:
+                                    otherprsn.status = "i"
+                                    otherprsn.infected_days = 0
+
+    def calculate_death(self):
+        for space in self.rooms:
+            for prsn in space.persons:
+                if prsn.status == "i":
+                    prsn.infected_days += 1
+                    if prsn.infected_days >= self.max_infected_time:
+                        if random.random() <= self.deathrate:
+                            prsn.status = "d"
+                        else:
+                            prsn.status = "c"
+
+
+
+    def time_step(self):
+        self.calculate_infected()
+        self.calculate_death()
 
 
 
@@ -71,8 +117,10 @@ while(True):
         print("fps: ", counter)
         #break'''
 
-    plt.waitforbuttonpress() #auskommentieren falls nicht erwünscht
+    #plt.waitforbuttonpress() #auskommentieren falls nicht erwünscht
     #insert some (positions/infections/ etc)- update function here
+    newscenario.time_step()
+    newscenario.update_room_axes()
     newscenario.update_scatters()
     fig.canvas.draw()
     fig.canvas.flush_events()
