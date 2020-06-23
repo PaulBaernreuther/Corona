d9f438d41ac12407f2d9d154688cc58c1d60737f
PaulBaernreuther <65441399+PaulBaernreuther@users.noreply.github.com>
2020-06-23 12:07:12 +0200

    Add files via upload

--- corona-Simulator.py
+++ corona-Simulator.py
@@ -7,75 +7,235 @@ import time
 import matplotlib.animation as anim
 from rooms import *
 
-
-
 class scenario:
-    def __init__(self):
-        self.number_of_rooms = 5
+    def __init__(self, number_infected = 5, deathrate = 0.1, max_infected_time = 20, infectionrate = 0.2, shape = (100,100), members = 2000, radius = 2):
+        self.number_of_rooms = 1
+        self.shape = shape
+        self.members = members
+        self.number_infected = number_infected
+        self.deathrate = deathrate
+        self.max_infected_time = max_infected_time
+        self.infectionrate = infectionrate
+        self.radius = radius
+
         self.rooms = []
         self.opt_arangement = 0,0
-        self.shape = (20,20)
-        self.members = 100
+        self.current_arangement = [0,0]
+        self.data = {"i": [], "v": [], "c": [], "d": []}
     def find_opt_arangement(self):
         a, b = fig.get_size_inches()
         return find_opt_arangement(self.number_of_rooms, ratio=(self.shape[1] * a / 2) / (self.shape[0] * b))
     def create_rooms(self):
         i,j = self.find_opt_arangement()
+        self.current_arangement = [i,j]
         for l in range(self.number_of_rooms):
-            newroom = room(self.shape)
+            newroom = Room(number_infected=self.number_infected, act_size = self.shape, members = self.members)
             newroom.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
             newroom.compute_scale(fig)
+            the_infected = random.sample(range(self.members), self.number_infected)
             for m in range(self.members):
-                person = Person(newroom)
+                person = Person(newroom, max_infected_time=self.max_infected_time, deathrate=self.deathrate, infectionrate= self.infectionrate, radius = self.radius)
+                if m in the_infected:
+                    person.status = "i"
                 person.register()
             self.rooms.append(newroom)
+    def new_room(self):
+        specialroom = Room(number_infected=self.number_infected, act_size = self.shape, members = self.members)
+        self.number_of_rooms += 1
+        i, j = self.find_opt_arangement()
+        for l, room in enumerate(self.rooms):
+            room.ax.remove()
+            room.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
+        l = self.number_of_rooms - 1
+        specialroom.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
+        self.current_arangement = [i, j]
+        self.rooms.append(specialroom)
+        return specialroom
+    def update_room_axes(self):
+        i,j = self.find_opt_arangement()
+        if i != self.current_arangement[0] or j != self.current_arangement[1]:
+            for l, room in enumerate(self.rooms):
+                room.ax.remove()
+                room.show_on_fig(fig, i, 2 * j, (1 + (l // j)) * j + l + 1)
+            self.current_arangement = [i, j]
     def draw_all_rooms(self):
         for thisroom in self.rooms:
             thisroom.draw()
     def update_scatters(self):
+        list_of_scatters = []
         for room in self.rooms:
             room.clear_room()
             room.compute_scale(fig)
             for person in room.persons:
+                person.keep_going()
                 #person.position = person.new_random_pos()
-                person.wiggle()
+                #person.wiggle()
                 person.register()
-            room.draw()
-            #fig.canvas.restore_region(room.axbackground)
-            #room.ax.draw_artist(room.scatter)
-            #room.ax.draw_artist(room.scatter2)
-            #fig.canvas.blit(room.ax.bbox)
+            room_scatters = room.draw()
+            for scat in room_scatters:
+                list_of_scatters.append(scat)
+        return list_of_scatters
+
+    def update_data(self):
+        for char in ["i", "v", "c", "d"]:
+            self.data[char].append(0)
+        for room in self.rooms:
+            room.update_data()
+            for char in ["i", "v", "c", "d"]:
+                self.data[char][-1] += room.data[char][-1]
+
+    def update_graph(self):
+        out = {}
+        for char in ["i", "v", "c", "d"]:
+            out[char] = np.array([0]+self.data[char]+[0])
+        return np.array([0]+[i for i in range(len(self.data["i"]))]+[len(self.data["i"])-1]), out
+
+    def update_relative_graph(self):
+        out = {}
+        size = (len(self.data["i"]))
+        for charlist in ["i", ["v","i"], ["c","v", "i"], ["d","c", "v", "i"]]:
+            out[charlist[0]] = np.array([0] + [sum([self.data[char][item] for char in charlist]) for item in range(size)] + [0])
+        return np.array([0] + [i for i in range(size)] + [len(self.data["i"]) - 1]), out
+
+    def calculate_infected(self):
+        for room in self.rooms:
+            room.calculate_infected()
+
+    def calculate_death(self):
+        for room in self.rooms:
+            room.calculate_death()
+
+    def time_step(self):
+        self.calculate_infected()
+        self.calculate_death()
+        self.update_data()
+
+    def jump(self, prsn, codomain):
+        prsn.room.persons.remove(prsn)
+        codomain.persons.append(prsn)
+        prsn.room = codomain
+        prsn.new_random_pos()
+        prsn.room.members -= 1
+        codomain.members += 1
 
 
 
+class ClusterScenario(scenario):
+    def __init__(self, jumpy_percentage = 0.01, jumptime = 10, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.jumpy_percentage = jumpy_percentage
+        self.jumptime = jumptime
+        self.time_since_jump = 0
 
-plt.ion()
+    def create_rooms(self):
+        super().create_rooms()
+        for room in self.rooms:
+            for prsn in room.persons:
+                prsn.jumpy = False
+                prsn.time_since_jump = int(random.random()*self.jumptime)
+        for room in self.rooms:
+            quantity = len(room.persons)
+            the_jumpies = random.sample(range(quantity), int(quantity*self.jumpy_percentage))
+            for number in the_jumpies:
+                room.persons[number].jumpy = True
+
+    def update_scatters(self):
+        list_of_jumpers =[]
+        for room in self.rooms:
+            for prsn in room.persons:
+                if prsn.jumpy:
+                    prsn.time_since_jump += 1
+                    if prsn.time_since_jump >= self.jumptime:
+                        prsn.time_since_jump = 0
+                        list_of_jumpers.append(prsn)
+        for jumper in list_of_jumpers:
+            self.jump(jumper, random.choice(self.rooms))
+        return super().update_scatters()
+
+
+class Supermarket(scenario):
+    def __init__(self, shopping_time = 7, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.shopping_time = shopping_time
+
+    def create_rooms(self):
+        super().create_rooms()
+        for room in self.rooms:
+            for prsn in room.persons:
+                prsn.purchase_interval = 50 + int(random.random() * 20 - 2)
+                prsn.time_since_purchase = int(random.random()*prsn.purchase_interval)
+                prsn.time_in_market = 0
+                prsn.room_of_origin = prsn.room
+        self.market = self.new_room()
+        self.market.ax.set_title("Supermercado")
+
+    def update_scatters(self):
+        list_of_thrifters = []
+        for room in self.rooms:
+            for prsn in room.persons:
+                if prsn.room is self.market:
+                    prsn.time_in_market += 1
+                    if prsn.time_in_market >= self.shopping_time:
+                        prsn.time_in_market = 0
+                        prsn.purchase_interval = 50 + int(random.random() * 20 - 2)
+                        self.jump(prsn, prsn.room_of_origin)
+                    continue
+                prsn.time_since_purchase += 1
+                if prsn.time_since_purchase >= prsn.purchase_interval:
+                    prsn.time_since_purchase = 0
+                    list_of_thrifters.append(prsn)
+        for prsn in list_of_thrifters:
+            self.jump(prsn, self.market)
+        return super().update_scatters()
+
+
 fig = plt.figure(figsize=(11,4))
 fig.patch.set_facecolor('#123456')
 fig.patch.set_alpha(0.7)
+ax = fig.add_subplot(2,2,1)
+
+
 
 k = 9
-newscenario = scenario()
+newscenario = Supermarket()
 newscenario.create_rooms()
 newscenario.draw_all_rooms()
 
+start = time.time()
+def update(frame_number):
+    newscenario.time_step()
+    newscenario.update_room_axes()
+    x, data = newscenario.update_relative_graph()
+    ax.clear()
+    for char in ["d", "c", "v", "i"]:
+        ax.fill(x, data[char], c = colors[char])
+    return newscenario.update_scatters()+[ax]
 
-t = time.time()
+
+animation = FuncAnimation(fig, update, interval=1, blit = True)
+
+plt.show()
+
+
+'''t = time.time()
 counter = 0
+
 
 fig.canvas.draw()
 while(True):
-    '''if time.time()-t < 1:
+    if time.time()-t < 1:
         counter += 1
     else:
         print("fps: ", counter)
-        #break'''
+        #break
 
-    plt.waitforbuttonpress() #auskommentieren falls nicht erwünscht
+    #plt.waitforbuttonpress() #auskommentieren falls nicht erwünscht
     #insert some (positions/infections/ etc)- update function here
+    newscenario.time_step()
+    newscenario.update_room_axes()
     newscenario.update_scatters()
     fig.canvas.draw()
-    fig.canvas.flush_events()
+    fig.canvas.flush_events()'''
 
 
 
--- rooms.py
+++ rooms.py
@@ -3,47 +3,95 @@ import numpy as np
 import random
 from functions import *
 
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
 radius_to_sice = 144
 radius = 0.5
+aura_on = True
+colors = {"i":'r',"v":'b',"c": 'g',"d": 'k'}
+cmap = ListedColormap(colors.values())
+norm = BoundaryNorm([0,1,2,3,4], cmap.N)
 
 
-class room:
-    def __init__(self, act_size = (3,4)):
+class Room:
+    def __init__(self, number_infected, act_size = (3,4), members = 100, max_infected_time = 10, deathrate = 10):
+        #plot-stuff
+        self.border = 2
         self.ax = None
         self.axbackground = None
         self.divider = None
         self.location_on_screen = (0.5,0)
         self.size_on_screen = (1,1)
         self.actual_size = act_size
+        if self.actual_size[0]<2*self.border or self.actual_size[1]<2*self.border:
+            raise ValueError("Everything is border")
         self.boxpos = (0,0)
-        self.members = 0
         self.scale = 1
-        self.border = 0.1
         self.draw_data = [[],[],[]]
         self.draw_data2 = [[],[],[]]
         self.scatter = None
         self.scatter2 = None
+
+        self.members = members
+        self.number_infected = number_infected
+
         self.persons = []
-    '''def get_divider(self, fig):
-        self.border = 0.1
-        while self.actual_size[0] < (self.size_on_screen[0]-2*self.border) and self.actual_size[1]<(self.size_on_screen[1]-2*self.border):
-            self.border += 0.1
-        x_factor= self.actual_size[0]/(self.size_on_screen[0]-2*self.border)
-        y_factor = self.actual_size[1]/(self.size_on_screen[1]-2*self.border)
-        self.scale = max(x_factor, y_factor)
-        draw_size = (self.actual_size[0]/self.scale, self.actual_size[1]/self.scale)
-        h = [Size.Fixed(self.border), Size.Fixed(draw_size[0])]
-        v = [Size.Fixed(self.border), Size.Fixed(draw_size[1])]
-        return Divider(fig, (self.location_on_screen[0], self.location_on_screen[1], 1, 1), h, v, aspect=False)
-    def init_ax(self ,fig):
-        h = [Size.Fixed(self.size_on_screen[0])]
-        v = [Size.Fixed(self.size_on_screen[1])]
-        self.divider = self.get_divider(fig)
-        self.ax = fig.add_axes(self.divider.get_position(),axes_locator=self.divider.new_locator(nx=1, ny=1))
-        #self.ax.get_xaxis().set_visible(False)
-        #self.ax.get_yaxis().set_visible(False)
-        self.ax.set_xlim(0, self.actual_size[0])
-        self.ax.set_ylim(0, self.actual_size[1])'''
+        self.data = {"i": [], "v": [], "c": [], "d": []}
+
+    def update_data(self):
+        for char in ["i", "v", "c", "d"]:
+            self.data[char].append(0)
+        for person in self.persons:
+            self.data[person.status][-1] += 1
+
+    def calculate_infected(self):
+        discrete_raster = [[[] for i in range(self.actual_size[0])] for j in range(self.actual_size[1])]
+        for person in self.persons:
+            x, y = int(person.position[0]), int(person.position[1])
+            discrete_raster[x][y].append(person)
+        pers_sorted_by_radius = sorted([person for person in self.persons if person.status in ["v", "i"]], key=lambda person: -person.radius)
+        for count, person in enumerate(pers_sorted_by_radius):
+            person.index = count
+        for person in pers_sorted_by_radius:
+            x, y = int(person.position[0]), int(person.position[1])
+            rad = person.radius
+            rad_int = int(rad)
+            i_min = max(x-rad_int, 0)
+            i_max = min(x+rad_int+1, int(self.actual_size[0])-1)
+            j_min = max(y-rad_int, 0)
+            j_max = min(y+rad_int+1, int(self.actual_size[1])-1)
+            index = person.index
+            current_radius = person.radius
+            curr_status = person.status
+            get_infected = False
+            for i in range(i_min, i_max):
+                for j in range(j_min, j_max):
+                    for otherperson in discrete_raster[i][j]:
+                        if otherperson.index >= index:
+                            continue
+                        distance = np.linalg.norm(np.array(person.position) - np.array(otherperson.position))
+                        if distance <= current_radius:
+                            if random.random() <= person.infectionrate:
+                                if curr_status == "i":
+                                    otherperson.status = "i"
+                                elif otherperson.status == "i":
+                                    get_infected = True
+            if get_infected:
+                person.status = "i"
+                person.infected_days = 0
+
+
+
+    def calculate_death(self):
+        for prsn in self.persons:
+            if prsn.status == "i":
+                prsn.infected_days += 1
+                if prsn.infected_days >= prsn.max_infected_time:
+                    if random.random() <= prsn.deathrate:
+                        prsn.status = "d"
+                    else:
+                        prsn.status = "c"
+
     def compute_scale(self, fig):
         a,b = get_ax_size(self.ax, fig)
         if self.actual_size[0]/self.actual_size[1] > a/b:
@@ -58,46 +106,81 @@ class room:
     def show_on_fig(self, fig, rows, cols, index):
         self.ax = fig.add_subplot(rows, cols, index, adjustable = "box", aspect = 1)
         self.axbackground = fig.canvas.copy_from_bbox(self.ax.bbox)
-        #self.ax.get_xaxis().set_visible(False)
-        #self.ax.get_yaxis().set_visible(False)
+        self.ax.get_xaxis().set_visible(False)
+        self.ax.get_yaxis().set_visible(False)
         self.ax.set_xlim(0, self.actual_size[0])
         self.ax.set_ylim(0, self.actual_size[1])
+        self.scatter = None
+        self.scatter2 = None
 
     def clear_room(self):
         self.draw_data = [[], [], []]
         self.draw_data2 = [[], [], []]
     def draw(self):
         if not self.scatter:
-            self.scatter = self.ax.scatter(self.draw_data[0], self.draw_data[1], c = self.draw_data[2], s = (radius*radius_to_sice)**2*self.scale**2)
-            self.scatter2 = self.ax.scatter(self.draw_data2[0], self.draw_data2[1], sizes = self.draw_data2[2], c = "r", alpha= 0.1)
+            self.scatter = self.ax.scatter(self.draw_data[0], self.draw_data[1], c = self.draw_data[2], s = (radius*radius_to_sice)**2*self.scale**2, cmap = cmap, norm = norm)
+            if aura_on:
+                self.scatter2 = self.ax.scatter(self.draw_data2[0], self.draw_data2[1], sizes = self.draw_data2[2], c = "r", alpha= 0.1)
         else:
             self.scatter.set_offsets(np.c_[self.draw_data[0], self.draw_data[1]])
             self.scatter.set_array(np.array(self.draw_data[2]))
             self.scatter.set_sizes(np.array([(radius*radius_to_sice)**2*self.scale**2 for item in range(len(self.draw_data[0]))]))
-            self.scatter2.set_offsets(np.c_[self.draw_data2[0], self.draw_data2[1]])
-            self.scatter2.set_sizes(np.array(self.draw_data2[2]))
+            if aura_on:
+                self.scatter2.set_offsets(np.c_[self.draw_data2[0], self.draw_data2[1]])
+                self.scatter2.set_sizes(np.array(self.draw_data2[2]))
+        if aura_on:
+            return [self.scatter, self.scatter2]
+        return [self.scatter]
 
 
 
 class Person:
-    def __init__(self, room):
-        self.status = random.choice(["v", "i", "c", "d"])
+    def __init__(self, room, infectionrate = 0.2, deathrate = 10, max_infected_time = 10, speed = 0.5, radius = 2):
+
         self.room = room
+        self.status = "v"
+        self.infected_days = 0
         self.position = self.new_random_pos()
-        self.radius = 2
+
+        self.max_infected_time = max_infected_time
+        self.deathrate = deathrate
+        self.infectionrate = infectionrate
+        self.speed = speed
+        self.radius = radius
+
 
         self.radius_anim_percentage = 1
         self.room.persons.append(self)
+        self.angle = random.random()
+        self.direction = [0,0]#uniform distr is wahrscheinlich nicht gut
+
     def new_random_pos(self):
-        return [0.1+np.random.random()*(self.room.actual_size[0]-0.2), 0.1+np.random.random()*(self.room.actual_size[1]-0.2)]
+        return [self.room.border+np.random.random()*(self.room.actual_size[0]-2*self.room.border), self.room.border+np.random.random()*(self.room.actual_size[1]-2*self.room.border)]
+    def keep_going(self):
+        if not (self.room.border<self.position[0]<self.room.actual_size[0]-self.room.border and self.room.border<self.position[1]<self.room.actual_size[1]-self.room.border):
+            angle_diff = 0.5
+        else:
+            angle_diff = np.random.normal(0, 0.1, 1)
+            if math.fabs(angle_diff)>0.5:
+                angle_diff = 0
+        self.angle = (self.angle + angle_diff) % 1
+        self.direction[0] = self.speed * math.cos(self.angle*2*math.pi)
+        self.direction[1] = self.speed * math.sin(self.angle*2*math.pi)
+        self.position = [self.position[0]+self.direction[0], self.position[1]+self.direction[1]]
     def wiggle(self):
         factor = 1
-        self.position[0] += np.random.random()*(factor)
-        self.position[1] += np.random.random()*(factor)
-        self.position[0] -= np.random.random()*(factor)
-        self.position[1] -= np.random.random()*(factor)
+        old_position = self.position
+        while True:
+            self.position[0] += np.random.random()*(factor)
+            self.position[1] += np.random.random()*(factor)
+            self.position[0] -= np.random.random()*(factor)
+            self.position[1] -= np.random.random()*(factor)
+            if (self.room.actual_size[0]>self.position[0]>0 and self.room.actual_size[1]>self.position[1]>0) or True:
+                break
+            print("we got to far")
+            self.position = old_position
     def register(self):
-        colors = {"v": 0.1, "i": 0.2, "c": 0.3, "d": 0.4}
+        colors = { "i": 0.5, "v": 1.5, "c": 2.5, "d": 3.5}
         color = colors[self.status]
         self.room.draw_data[0].append(self.position[0])
         self.room.draw_data[1].append(self.position[1])
